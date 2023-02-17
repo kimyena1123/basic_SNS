2fda6c4e3a701128c8210558fb32accaf58437e5
kimyena1123 <kyq104@gmail.com>
2023-02-17 19:18:24 +0900

    feat: 디렉토리 생성 실패 로그 추가

--- src/main/java/com/yena/sns/common/FileManagerService.java
+++ src/main/java/com/yena/sns/common/FileManagerService.java
@@ -6,6 +6,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 public class FileManagerService {
@@ -13,7 +15,9 @@ public class FileManagerService {
 	//파일 저장
 	//파일을 저장하려면 얻니에 저장할 지 설정해야 한다.
 	public static final String PILE_UPLOAD_PATH = "C:\\java_kyn\\SpringProject\\upload\\images\\sns";
-
+	
+	private static Logger logger = LoggerFactory.getLogger(FileManagerService.class);
+	
 	//파일 저장을 위한 메소드
 	//파일을 저장하고 client에서 접근 가능한 주소를 만들어 리턴하는 기능
 	//static: 객체 생성 없이 메소드를 호출하기 위해
@@ -34,24 +38,35 @@ public class FileManagerService {
 		//디렉토리 생성
 		File directory = new File(directoryPath);
 		
-		if(directory.mkdir() == false) { //디렉토리 생성 실패
+		if(directory.mkdir() == false) { 
+			//디렉토리 생성 실패
+			
+			//디렉토리 생성 실패 로그 추가
+			logger.error("savefile : 디렉토리 생성 실패 " + directoryPath);
 			return null;
 		}
+	
 		
 		//파일 저장
 		//바이트 단위로 꺼내야 함
+		
+		String filePath = null;
+		
 		try {
 			byte[] bytes = file.getBytes();
 			
 			//fileUploadPath//userId_현재시간//사진이름.png
 			//directoryPath = PILE_UPLOAD_PATH + directoryName;
-			String filePath = directoryPath + file.getOriginalFilename();
+			filePath = directoryPath + file.getOriginalFilename();
 			Path path = Paths.get(filePath);
 			Files.write(path, bytes);
 			
 		} catch (IOException e) {
 			
 			e.printStackTrace();
+			
+			//디렉토리 생성 실패 로그 추가
+			logger.error("saveFile : 파일 생성 에러 - " + filePath);
 			
 			return null;
 		}
